1c6d9c940715ff6c5ce56a6db2a2d3c1ae6f00cb
RRBarman <48653401+RRBarman@users.noreply.github.com>
2022-09-13 13:12:31 +0530

    Add files via upload

---
+++ Area of Square/Main.java
@@ -0,0 +1,15 @@
+package NewProg;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+	    int a = 0;
+	    a = sc.nextInt();
+	    System.out.println(Integer.toString(a*a));
+	    sc.close();
+
+	}
+
+}

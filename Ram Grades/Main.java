544d7feefe68aa7cd677d7def6092c9bfd938194
RRBarman <48653401+RRBarman@users.noreply.github.com>
2022-09-13 13:16:49 +0530

    Add files via upload

---
+++ Ram Grades/Main.java
@@ -0,0 +1,18 @@
+package NewProg;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int sum = a+b+c+d;
+        int f = sum/4;
+        System.out.println(f);
+        sc.close();
+	}
+
+}
